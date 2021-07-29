a75edd87c6602a9077882a91f368bbc84f8c98a1
yoonbitnara <yoonbitnara@gmail.com>
2021-07-30 00:43:24 +0900

    resultpage test

--- js/kakaoshare.js
+++ js/kakaoshare.js
@@ -1,34 +1,33 @@
-function kakao(){
-    Kakao.Link.sendDefault({
-        objectType: 'feed',
-        content: {
-        title: '디저트 사진',
-        description: '아메리카노, 빵, 케익',
-        imageUrl:
-            'http://mud-kage.kakao.co.kr/dn/NTmhS/btqfEUdFAUf/FjKzkZsnoeE4o19klTOVI1/openlink_640x640s.jpg',
+const url = 'https://www.brand-test.com/';
+
+function setShare(){
+  var resultImg = document.querySelector('#resultImg');
+  var resultAlt = resultImg.firstElementChild.alt;
+  const shareTitle = '나에게 어울리는 브랜드 결과';
+  const shareDes = infoList[resultAlt].name;
+  const shareImage = url + 'img/image-' + resultAlt + '.png';
+  const shareURL = url + 'page/result-' + resultAlt + '.html';
+
+  Kakao.Link.sendDefault({
+    objectType: 'feed',
+    content: {
+      title: shareTitle,
+      description: shareDes,
+      imageUrl: shareImage,
+      link: {
+        mobileWebUrl: shareURL,
+        webUrl: shareURL
+      },
+    },
+
+    buttons: [
+      {
+        title: '결과확인하기',
         link: {
-            mobileWebUrl: 'https://developers.kakao.com',
-            androidExecutionParams: 'test',
+          mobileWebUrl: shareURL,
+          webUrl: shareURL,
         },
-        },
-        social: {
-        likeCount: 10,
-        commentCount: 20,
-        sharedCount: 30,
-        },
-        buttons: [
-        {
-            title: '웹으로 이동',
-            link: {
-            mobileWebUrl: 'https://developers.kakao.com',
-            },
-        },
-        {
-            title: '앱으로 이동',
-            link: {
-            mobileWebUrl: 'https://developers.kakao.com',
-            },
-        },
-        ]
-    });
+      },
+    ]
+  });
 }
